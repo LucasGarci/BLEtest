8e69fd92679be92e89ff7361d3fd63fe8c993958
LucasGarci <lucasgarcifer@gmail.com>
2018-06-04 23:52:23 +0200

    added colorwheel (#12)

--- app/App.js
+++ app/App.js
@@ -1,7 +1,4 @@
 import React from "react"
-import { StyleSheet, Text, View, NativeModules } from "react-native"
-import { BleManager } from "react-native-ble-plx"
-import { createStackNavigator } from "react-navigation"
 import { Root } from "./navigation/router"
 
 class App extends React.Component {
--- app/containers/DeviceTabs.js
+++ app/containers/DeviceTabs.js
@@ -1,5 +1,4 @@
 import React from "react"
-import { StyleSheet, Text, View, NativeModules } from "react-native"
 import { DeviceNavigation } from "../navigation/router"
 
 export class DeviceTabs extends React.Component {
