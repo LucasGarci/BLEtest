b6161765fb25e79d572ef90a53f2a645c77e55c9
LucasGarci <lucasgarcifer@gmail.com>
2018-06-09 20:47:57 +0200

    aniadido el card a bluetest y a options y el switch (#19)

--- app/components/DeviceItem.js
+++ app/components/DeviceItem.js
@@ -1,36 +1,35 @@
-import React from 'react';
-import { StyleSheet, Text, View, Button, TouchableOpacity } from 'react-native';
-import { BleManager } from 'react-native-ble-plx';
-import { Root } from "../navigation/router"
-
+import React from "react";
+import { StyleSheet, Text, View, Button, TouchableOpacity } from "react-native";
+import { BleManager } from "react-native-ble-plx";
+import { Card } from "react-native-elements";
 
 export class DeviceItem extends React.Component {
-    constructor() {
-        super()
-        this.state = {
-            id: null,
-            name: null,
-            rssi: null,
-            isConnectable: null,
-        }
-        this.manager = new BleManager();
-        this.device = {}
-    }
+  constructor() {
+    super();
+    this.state = {
+      id: null,
+      name: null,
+      rssi: null,
+      isConnectable: null
+    };
+    this.manager = new BleManager();
+    this.device = {};
+  }
 
-    componentDidMount() {
-        const { device } = this.props
-        this.setState({
-            id: device.id,
-            name: device.name || 'Anonimo',
-            rssi: device.rssi,
-            isConnectable: device.isConnectable,
-        })
-        this.device = this.props.device;
-    }
+  componentDidMount() {
+    const { device } = this.props;
+    this.setState({
+      id: device.id,
+      name: device.name || "Anonimo",
+      rssi: device.rssi,
+      isConnectable: device.isConnectable
+    });
+    this.device = this.props.device;
+  }
 
-    onPress = () => {
-        this.props.navigation.navigate("Device")
-        /*this.manager.connectToDevice(this.state.id)
+  onPress = () => {
+    this.props.navigation.navigate("Device");
+    /*this.manager.connectToDevice(this.state.id)
             .then((device) => {
                 device.discoverAllServicesAndCharacteristics().then((result) => {
                     console.log({ fullData: result })
@@ -40,33 +39,25 @@ export class DeviceItem extends React.Component {
                 console.log({ result: reason }); // Error!
             })
         */
-    };
+  };
 
-    render() {
-        return (
-            <TouchableOpacity style={styles.list} onPress={this.onPress}>
-                <View>
-                    <Text>
-                        Id: {this.state.id}
-                    </Text>
-                    <Text>
-                        Name: {this.state.name}
-                    </Text>
-                    <Text>
-                        Rssi: {this.state.rssi}
-                    </Text>
-                </View>
-            </TouchableOpacity>
-        );
-    }
+  render() {
+    return (
+      <TouchableOpacity style={{ padding: 1 }} onPress={this.onPress}>
+        <Card containerStyle={styles.list}>
+          <Text>Id: {this.state.id}</Text>
+          <Text>Name: {this.state.name}</Text>
+          <Text>Rssi: {this.state.rssi}</Text>
+        </Card>
+      </TouchableOpacity>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    list: {
-        margin: 1,
-        padding: 2,
-        borderWidth: 1,
-        borderRadius: 4,
-        borderColor: '#505050',
-    },
-})
+  list: {
+    padding: 7,
+    margin: 10,
+    borderRadius: 10,
+  }
+});
--- app/containers/OptionsScreen.js
+++ app/containers/OptionsScreen.js
@@ -1,71 +1,96 @@
 import React from "react";
-import { StyleSheet, Text, View, Picker } from "react-native";
+import { StyleSheet, Text, View, Picker, Switch } from "react-native";
+import { Card } from "react-native-elements";
 
 export class OptionsScreen extends React.Component {
   constructor(props) {
     super();
     this.state = {
-      language: "Spanish"
+      language: "Spanish",
+      switchValue: true,
+      colorScheme: "izquierda"
     };
   }
   onPress = () => {
     console.log("OpptionsTab");
   };
 
+  onSwitch(value) {
+    value
+      ? this.setState({ switchValue: value, colorScheme: "derecha" })
+      : this.setState({ switchValue: value, colorScheme: "izquierda" });
+  }
+
   render() {
     return (
       <View style={styles.centerContainer}>
-        <View style={styles.optionContainer}>
-          <View style={styles.descriptionContainer}>
-            <Text style={styles.textOption}>Seleccionar idioma: </Text>
-          </View>
-          <View style={styles.switchContainer}>
-            <Picker
-              selectedValue={this.state.language}
-              style={{ height: 50, width: 100 }}
-              onValueChange={(itemValue, itemIndex) => {
-                this.setState({ language: itemValue });
-                console.log(this.state.language)
-              }}
-            >
-              <Picker.Item label="Español" value="es" />
-              <Picker.Item label="English" value="en" />
-            </Picker>
-          </View>
-        </View>
-
-        <View style={styles.optionContainer}>
-          <View style={styles.descriptionContainer}>
-            <Text style={styles.textOption}>Borrar datos locales:</Text>
-          </View>
-          <View style={styles.switchContainer}>
-            <Text style={styles.textOption}>Button</Text>
-          </View>
-        </View>
-
-        <View style={styles.optionContainer}>
-          <View style={styles.descriptionContainer}>
-            <Text style={styles.textOption}>Esquema de colores:</Text>
-          </View>
-          <View style={styles.switchContainer}>
-            <Text style={styles.textOption}>Switch</Text>
-          </View>
-        </View>
-
-        <View style={styles.footer}>
+        <Card containerStyle={styles.list}>
           <View style={styles.optionContainer}>
             <View style={styles.descriptionContainer}>
-              <Text style={styles.textOption}>Salir de la aplicación</Text>
+              <Text style={styles.textOption}>Seleccionar idioma: </Text>
             </View>
             <View style={styles.switchContainer}>
-              <Text style={styles.textOption}>Bye!!</Text>
+              <Picker
+                selectedValue={this.state.language}
+                style={{ height: 50, width: 100 }}
+                onValueChange={(itemValue, itemIndex) => {
+                  this.setState({ language: itemValue });
+                  console.log(this.state.language);
+                }}
+              >
+                <Picker.Item label="Español" value="es" />
+                <Picker.Item label="English" value="en" />
+              </Picker>
             </View>
           </View>
+        </Card>
+
+        <Card containerStyle={styles.list}>
           <View style={styles.optionContainer}>
             <View style={styles.descriptionContainer}>
-              <Text style={styles.link}>Averigua como me han hecho ;)</Text>
+              <Text style={styles.textOption}>Borrar datos locales:</Text>
+            </View>
+            <View style={styles.switchContainer}>
+              <Text style={styles.textOption}>Button</Text>
             </View>
           </View>
+        </Card>
+
+        <Card containerStyle={styles.list}>
+          <View style={styles.optionContainer}>
+            <View style={styles.descriptionContainer}>
+              <Text style={styles.textOption}>
+                Esquema de colores: {this.state.colorScheme}
+              </Text>
+            </View>
+            <View style={styles.switchContainer}>
+              <Switch
+                onValueChange={value => this.onSwitch(value)}
+                value={this.state.switchValue}
+              />
+            </View>
+          </View>
+        </Card>
+
+        <View style={styles.footer}>
+          <Card containerStyle={styles.list}>
+            <View style={styles.optionContainer}>
+              <View style={styles.descriptionContainer}>
+                <Text style={styles.textOption}>Salir de la aplicación</Text>
+              </View>
+              <View style={styles.switchContainer}>
+                <Text style={styles.textOption}>Bye!!</Text>
+              </View>
+            </View>
+          </Card>
+
+          <Card containerStyle={styles.list}>
+            <View style={styles.optionContainer}>
+              <View style={styles.descriptionContainer}>
+                <Text style={styles.link}>Averigua como me han hecho ;)</Text>
+              </View>
+            </View>
+          </Card>
         </View>
       </View>
     );
@@ -77,13 +102,13 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: "column",
     margin: 5,
-    marginTop: 5,
+    marginTop: 10,
+    marginBottom: 10,
     borderRadius: 10
   },
   optionContainer: {
     flexDirection: "row",
     justifyContent: "center",
-    margin: 5,
     height: 70,
     borderRadius: 10,
     backgroundColor: "#fff"
@@ -101,9 +126,9 @@ const styles = StyleSheet.create({
   },
   footer: {
     flex: 1,
-    flexDirection: "column",
-    alignItems: "flex-end",
-    justifyContent: "flex-end"
+    paddingBottom: 1,
+    justifyContent: "flex-end",
+    flexDirection: "column"
   },
   textOption: {
     fontSize: 16,
@@ -113,5 +138,10 @@ const styles = StyleSheet.create({
   link: {
     fontSize: 19,
     color: "#3366fB"
+  },
+  list: {
+    padding: 0,
+    margin: 8,
+    borderRadius: 10
   }
 });
