296ef1e5c17bd9f6e3d7403d147cdaffd92571f0
unknown <lucasgarcifer@gmail.com>
2018-06-10 19:55:08 +0200

    some stetic changes done

--- app/components/PrefabPicker.js
+++ app/components/PrefabPicker.js
@@ -1,65 +1,97 @@
-import React, { Component } from 'react';
-import { StyleSheet, AppRegistry, View, TouchableOpacity, Text } from 'react-native';
+import React, { Component } from "react";
+import { StyleSheet, View, TouchableOpacity, Text } from "react-native";
+import { Card } from "react-native-elements";
 
 export class PrefabPicker extends Component {
-    constructor() {
-        super()
-        this.state = {
-            color1: "#aa6822",
-            color2: "#006385",
-            color3: "#fff",
-            color4: "#ee0000",
-            brigthness1: 100,
-            brigthness2: 70,
-            brigthness3: 9,
-            brigthness4: 42,
-        }
-    }
-    onPress = () => {
-        console.log('Pulsado')
-    }
-    render() {
-        return (
-            <View style={{
-              //  flex: 1,
-                flexDirection: 'row',
-                justifyContent: 'space-between',
-            }}>
-                <View style={styles.pickerItem} >
-                    <TouchableOpacity style= {{flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor:this.state.color1}} onPress={this.onPress}>
-                        <Text>{JSON.stringify(this.state.brigthness1)}</Text>
-                    </TouchableOpacity>
-                </View>
-                <View style={styles.pickerItem} >
-                    <TouchableOpacity style= {{flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor:this.state.color2}} onPress={this.onPress}>
-                        <Text>{JSON.stringify(this.state.brigthness2)}</Text>
-                    </TouchableOpacity>
-                </View>
-                <View style={styles.pickerItem} >
-                    <TouchableOpacity style= {{flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor:this.state.color3}} onPress={this.onPress}>
-                        <Text>{JSON.stringify(this.state.brigthness3)}</Text>
-                    </TouchableOpacity>
-                </View>
-                <View style={styles.pickerItem} >
-                    <TouchableOpacity style= {{flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor:this.state.color4}} onPress={this.onPress}>
-                        <Text>{JSON.stringify(this.state.brigthness4)}</Text>
-                    </TouchableOpacity>
-                </View>
-            </View >
-        );
-    }
-};
-
+  constructor() {
+    super();
+    this.state = {
+      color1: "#ff0000",
+      color2: "#00ff00",
+      color3: "#2020ff",
+      color4: "#ffff00"
+    };
+  }
+  onPress = () => {
+    console.log("Pulsado");
+  };
+  render() {
+    return (
+      <View
+        style={{
+          //  flex: 1,
+          flexDirection: "row",
+          justifyContent: "space-between"
+        }}
+      >
+        <View style={styles.pickerItem}>
+          <TouchableOpacity
+            style={{
+              flex: 1,
+              alignItems: "center",
+              justifyContent: "center",
+              backgroundColor: this.state.color1,
+              borderRadius: 45
+            }}
+            onPress={this.onPress}
+          >
+            <Text>{JSON.stringify(this.state.color1)}</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.pickerItem}>
+          <TouchableOpacity
+            style={{
+              flex: 1,
+              alignItems: "center",
+              justifyContent: "center",
+              backgroundColor: this.state.color2,
+              borderRadius: 45
+            }}
+            onPress={this.onPress}
+          >
+            <Text>{JSON.stringify(this.state.color2)}</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.pickerItem}>
+          <TouchableOpacity
+            style={{
+              flex: 1,
+              alignItems: "center",
+              justifyContent: "center",
+              backgroundColor: this.state.color3,
+              borderRadius: 45
+            }}
+            onPress={this.onPress}
+          >
+            <Text>{JSON.stringify(this.state.color3)}</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.pickerItem}>
+          <TouchableOpacity
+            style={{
+              flex: 1,
+              alignItems: "center",
+              justifyContent: "center",
+              backgroundColor: this.state.color4,
+              borderRadius: 45
+            }}
+            onPress={this.onPress}
+          >
+            <Text>{JSON.stringify(this.state.color4)}</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
-    pickerItem: {
-        width: 55,
-        height: 55,
-        margin: 5,
-        borderWidth: 1,
-        borderRadius: 2,
-    },
-    touchItem: {
-        
-    }
-})
+  pickerItem: {
+    width: 70,
+    height: 70,
+    margin: 10,
+    borderWidth: 2,
+    borderRadius: 45
+  },
+  touchItem: {}
+});
