af22afe03921aa5b98ae0b60050cf8dc785fe501
unknown <lucasgarcifer@gmail.com>
2018-06-11 20:57:33 +0200

    disabled yellow box

--- app/Example.js
+++ app/Example.js
@@ -23,7 +23,7 @@ const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
 const BleManagerModule = NativeModules.BleManager;
 const bleManagerEmitter = new NativeEventEmitter(BleManagerModule);
 
-export default class App extends Component {
+export class Example extends Component {
   constructor(){
     super()
 
