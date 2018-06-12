eabfa6f4c5d2febe6c77bcde05a45403f18a688c
Desfase <kikdesfase@gmail.com>
2018-06-12 23:39:50 +0200

    trying to connect to bombi

--- app/Scanner.js
+++ app/Scanner.js
@@ -1,145 +1,213 @@
-import React from "react"
+import React from "react";
 import {
-    ImageBackground,
-    StyleSheet,
-    Text,
-    View,
-    ScrollView,
-    Button,
-    FlatList,
-    AppRegistry,
-    TouchableHighlight,
-    NativeAppEventEmitter,
-    NativeEventEmitter,
-    NativeModules,
-    Platform,
-    PermissionsAndroid,
-    ListView,
-    AppState,
-    Dimensions,
-} from 'react-native';
-import BleManager from 'react-native-ble-manager';
-import { DeviceItem } from "./components/DeviceItem"
+  ImageBackground,
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  Button,
+  FlatList,
+  AppRegistry,
+  TouchableHighlight,
+  NativeAppEventEmitter,
+  NativeEventEmitter,
+  NativeModules,
+  Platform,
+  PermissionsAndroid,
+  ListView,
+  AppState,
+  Dimensions
+} from "react-native";
+import BleManager from "react-native-ble-manager";
+import { DeviceItem } from "./components/DeviceItem";
 
+//We get the class to subscribe
 const BleManagerModule = NativeModules.BleManager;
+//We create our events emitter
 const bleManagerEmitter = new NativeEventEmitter(BleManagerModule);
 
 export class Scanner extends React.Component {
-    constructor() {
-        super()
-        this.state = {
-            scanning: false,
-            bleState: " ",
-            appState: " ",
-            devicesIds: [],
-            devices: []
-        }
-        this.isDiscovering = false
+  constructor() {
+    super();
+    this.state = {
+      scanning: false,
+      bleState: "",
+      appState: "",
+      devicesIds: [],
+      devices: [],
+      devicesConnectedIds: [],
+      devicesConnected: []
+    };
 
-        this.handleDiscoverPeripheral = this.handleDiscoverPeripheral.bind(this);
-        this.handleStopScan = this.handleStopScan.bind(this);
-        this.handleUpdateValueForCharacteristic = this.handleUpdateValueForCharacteristic.bind(this);
-        this.handleDisconnectedPeripheral = this.handleDisconnectedPeripheral.bind(this);
-        this.handleAppStateChange = this.handleAppStateChange.bind(this);
+    this.handleDiscoverPeripheral = this.handleDiscoverPeripheral.bind(this);
+    this.handleStopScan = this.handleStopScan.bind(this);
+    this.handleUpdateValueForCharacteristic = this.handleUpdateValueForCharacteristic.bind(
+      this
+    );
+    this.handleDisconnectedPeripheral = this.handleDisconnectedPeripheral.bind(
+      this
+    );
+    this.handleAppStateChange = this.handleAppStateChange.bind(this);
+  }
+
+  handleDiscoverPeripheral(device) {
+    if (!this.state.devicesIds.includes(device.id)) {
+      console.log(device);
+      this.setState({ devicesIds: [...this.state.devicesIds, device.id] });
+      // Añadimos el dispositivo completo a la lista
+      this.setState({
+        devices: [...this.state.devices, device]
+      });
     }
+  }
 
-    handleDiscoverPeripheral(device) {
-        
-        var devices = this.state.devices;
-        if (!this.state.devicesIds.includes(device.id)) {
-            console.log(device)
-            this.setState({ devicesIds: [...this.state.devicesIds, device.id] });
-            // Añadimos el dispositivo completo a la lista
-            this.setState({
-                devices: [...this.state.devices, device]
-            });
-        }
+  handleAppStateChange() {}
+
+  componentWillUnmount() {
+    BleManager.stopScan().then(() => {
+      // Success code
+      console.log("Scan stopped on Unmount");
+    });
+    this.handlerDiscover.remove();
+    this.handlerStop.remove();
+    this.handlerDisconnect.remove();
+    this.handlerUpdate.remove();
+  }
+
+  handleDisconnectedPeripheral() {}
+
+  handleUpdateValueForCharacteristic() {}
+
+  handleStopScan() {}
+
+  componentDidMount() {
+    // We set listeners to our events to our api events
+    this.handlerDiscover = bleManagerEmitter.addListener(
+      "BleManagerDiscoverPeripheral",
+      this.handleDiscoverPeripheral
+    );
+    this.handlerStop = bleManagerEmitter.addListener(
+      "BleManagerStopScan",
+      this.handleStopScan
+    );
+    this.handlerDisconnect = bleManagerEmitter.addListener(
+      "BleManagerDisconnectPeripheral",
+      this.handleDisconnectedPeripheral
+    );
+    this.handlerUpdate = bleManagerEmitter.addListener(
+      "BleManagerDidUpdateValueForCharacteristic",
+      this.handleUpdateValueForCharacteristic
+    );
+    //Start manager
+    BleManager.start();
+    //Comprobamos el BLE (nos pide permisos)
+    BleManager.enableBluetooth()
+      .then(() => {
+        // Success code
+        console.log("The bluetooth is already enabled or the user confirm");
+      })
+      .catch(error => {
+        // Failure code
+        console.warn({ error });
+        console.log("The user refuse to enable bluetooth");
+      });
+    this.retrieveConnected();
+  }
+
+  retrieveConnected() {
+    BleManager.getConnectedPeripherals([]).then(results => {
+      console.log({ ConnectedDevices: results });
+      for (var i = 0; i < results.length; i++) {
+        var device = results[i];
+        device.connected = true;
+        this.setState({ devicesConnected: results });
+      }
+    });
+  }
+
+  resetDevices() {
+    // Reseteamos los devices y los ids
+    this.setState({ devicesIds: [] });
+    this.setState({ devices: [] });
+    this.setState({ devicesConnectedIds: [] });
+    this.setState({ devicesConnected: [] });
+  }
+
+  startStop() {
+    // Limpiamos y empezamos nuevo escaner o detenemos el actual
+    if (this.state.scanning) {
+      console.log("Stoped scanner");
+      BleManager.stopScan().then(() => {
+        // Success code
+        console.log("Scan stopped");
+      });
+      this.setState({ scanning: false });
+    } else {
+      BleManager.scan([], 5, true).then(result => {
+        console.log({ result });
+        console.log("Scan started");
+      });
+      this.setState({ scanning: true });
+      this.resetDevices();
     }
+    this.retrieveConnected();
+  }
 
-    handleAppStateChange() {
-    }
-
-    componentWillUnmount() {
-        this.handlerDiscover.remove();
-        this.handlerStop.remove();
-        this.handlerDisconnect.remove();
-        this.handlerUpdate.remove();
-    }
-
-    handleDisconnectedPeripheral() {
-    }
-
-    handleUpdateValueForCharacteristic() {
-    }
-
-    handleStopScan() { }
-
-    componentDidMount() {
-        
-        this.handlerDiscover = bleManagerEmitter.addListener('BleManagerDiscoverPeripheral', this.handleDiscoverPeripheral);
-        this.handlerStop = bleManagerEmitter.addListener('BleManagerStopScan', this.handleStopScan);
-        this.handlerDisconnect = bleManagerEmitter.addListener('BleManagerDisconnectPeripheral', this.handleDisconnectedPeripheral);
-        this.handlerUpdate = bleManagerEmitter.addListener('BleManagerDidUpdateValueForCharacteristic', this.handleUpdateValueForCharacteristic);
-        BleManager.start();
-    }
-
-    resetDevices() {
-        // Reseteamos los devices y los ids
-        this.setState({ devicesIds: [] })
-        this.setState({ devices: [] })
-    }
-
-    startStop() {
-        // Limpiamos y empezamos nuevo escaner o detenemos el actual
-        if (this.state.isDiscovering) {
-            console.log("Stoped scanner")
-
-            this.setState({ isDiscovering: false })
-        } else {
-            BleManager.scan([], 5, false)
-                .then((result) => {
-                    console.log({ result });
-                    console.log('Scan started');
-                });
-            this.setState({ isDiscovering: true })
-            this.resetDevices()
-        }
-    }
-
-    render() {
-        return (
-            <ImageBackground
-                source={require('./img/fondoapp.png')}
-                style={{ width: '100%', height: '100%' }} >
-                <View>
-                    <View>
-                        <Button
-                            // Propiedades del botón ("props")
-                            title={this.state.isDiscovering ? "Stop scanner" : "Start new scan"}
-                            onPress={() => {
-                                this.startStop()
-                            }}
-                        />
-                        <ScrollView style={styles.container}>
-                            <Text>Nº Devices found: {this.state.devicesIds.length}</Text>
-                            <FlatList
-                                // Le pasamos el array de dispositivos de nuetro estado
-                                data={this.state.devices}
-                                renderItem={({ item }) => (
-                                    <DeviceItem device={item} navigation={this.props.navigation} />
-                                )}
-                            />
-                        </ScrollView>
-                    </View>
-                </View>
-            </ImageBackground>
-        )
-    }
+  render() {
+    return (
+      <ImageBackground
+        source={require("./img/fondoapp.png")}
+        style={{ width: "100%", height: "100%" }}
+      >
+        <View>
+          <View>
+            <Button
+              // Propiedades del botón ("props")
+              title={this.state.scanning ? "Stop scanner" : "Start new scan"}
+              onPress={() => {
+                this.startStop();
+              }}
+            />
+            <ScrollView style={styles.container}>
+              <Text>
+                Nº Dispositivos Conectados: {this.state.devicesConnected.length}
+              </Text>
+              <FlatList
+                // Le pasamos el array de dispositivos de nuetro estado
+                data={this.state.devicesConnected}
+                renderItem={({ item }) => (
+                  <DeviceItem
+                    device={item}
+                    navigation={this.props.navigation}
+                    parent={this}
+                  />
+                )}
+              />
+              <Text>
+                Nº Dispositivos Encontrados: {this.state.devicesIds.length}
+              </Text>
+              <FlatList
+                // Le pasamos el array de dispositivos de nuetro estado
+                data={this.state.devices}
+                renderItem={({ item }) => (
+                  <DeviceItem
+                    device={item}
+                    navigation={this.props.navigation}
+                    parent={this}
+                  />
+                )}
+              />
+            </ScrollView>
+          </View>
+        </View>
+      </ImageBackground>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    container: {
-        padding: 5,
-        borderColor: "#d6d7da"
-    }
-})
+  container: {
+    padding: 5,
+    borderColor: "#d6d7da"
+  }
+});
--- app/components/DeviceItem.js
+++ app/components/DeviceItem.js
@@ -1,7 +1,7 @@
 import React from "react";
-import { StyleSheet, Text, View, Button, TouchableOpacity } from "react-native";
-import { BleManager } from "react-native-ble-plx";
+import { StyleSheet, Text, TouchableOpacity } from "react-native";
 import { Card } from "react-native-elements";
+import BleManager from "react-native-ble-manager";
 
 export class DeviceItem extends React.Component {
   constructor() {
@@ -10,9 +10,9 @@ export class DeviceItem extends React.Component {
       id: null,
       name: null,
       rssi: null,
-      isConnectable: null
+      connected: false,
+      peripherals: new Map()
     };
-    this.manager = new BleManager();
     this.device = {};
   }
 
@@ -22,31 +22,153 @@ export class DeviceItem extends React.Component {
       id: device.id,
       name: device.name || "Anonimo",
       rssi: device.rssi,
+      connected: device.connected,
     });
     this.device = this.props.device;
   }
 
-  onPress = () => {
-    this.props.navigation.navigate("Device");
-    /*this.manager.connectToDevice(this.state.id)
-            .then((device) => {
-                device.discoverAllServicesAndCharacteristics().then((result) => {
-                    console.log({ fullData: result })
-                })
-            })
-            .catch((reason) => {
-                console.log({ result: reason }); // Error!
-            })
-        */
+  _onPress = () => {
+    const peripheral = this.props.device
+    if (peripheral) {
+      console.log("Connected to " + peripheral.id);
+      if (peripheral.connected) {
+        BleManager.disconnect(peripheral.id);
+      } else {
+        BleManager.connect(peripheral.id)
+          .then(() => {
+            let peripherals = this.state.peripherals;
+            let p = peripherals.get(peripheral.id);
+            if (p) {
+              p.connected = true;
+              peripherals.set(peripheral.id, p);
+              this.setState({ peripherals });
+            }
+            console.log("Connected to " + peripheral.id);
+
+            setTimeout(() => {
+              /* Test read current RSSI value
+            BleManager.retrieveServices(peripheral.id).then((peripheralData) => {
+              console.log('Retrieved peripheral services', peripheralData);
+              BleManager.readRSSI(peripheral.id).then((rssi) => {
+                console.log('Retrieved actual RSSI value', rssi);
+              });
+            });*/
+
+              // Test using bleno's pizza example
+              // https://github.com/sandeepmistry/bleno/tree/master/examples/pizza
+              BleManager.retrieveServices(peripheral.id).then(
+                peripheralInfo => {
+                  console.log({peripheralInfo});
+                  var service = "13333333-3333-3333-3333-333333333337";
+                  var bakeCharacteristic =
+                    "13333333-3333-3333-3333-333333330003";
+                  var crustCharacteristic =
+                    "13333333-3333-3333-3333-333333330001";
+
+                  setTimeout(() => {
+                    BleManager.startNotification(
+                      peripheral.id,
+                      service,
+                      bakeCharacteristic
+                    )
+                      .then(() => {
+                        console.log("Started notification on " + peripheral.id);
+                        setTimeout(() => {
+                          BleManager.write(
+                            peripheral.id,
+                            service,
+                            crustCharacteristic,
+                            [0]
+                          ).then(() => {
+                            console.log("Writed NORMAL crust");
+                            BleManager.write(
+                              peripheral.id,
+                              service,
+                              bakeCharacteristic,
+                              [1, 95]
+                            ).then(() => {
+                              console.log(
+                                "Writed 351 temperature, the pizza should be BAKED"
+                              );
+                              /*
+                        var PizzaBakeResult = {
+                          HALF_BAKED: 0,
+                          BAKED:      1,
+                          CRISPY:     2,
+                          BURNT:      3,
+                          ON_FIRE:    4
+                        };*/
+                            });
+                          });
+                        }, 500);
+                      })
+                      .catch(error => {
+                        console.log("Notification error", error);
+                      });
+                  }, 200);
+                }
+              );
+            }, 900);
+          })
+          .catch(error => {
+            console.log("Connection error", error);
+          });
+      }
+    }
+    /*  BleManager.stopScan().then(() => {
+      // Success code
+      console.log("Scan stopped");
+    });
+
+    if (this.props.device.connected) {
+      console.log("Device is connected");
+      BleManager.retrieveServices(this.device.id)
+        .then(deviceInfo => {
+          // Success code
+          console.log({
+            DeviceInfo: deviceInfo
+          });
+          this.props.navigation.navigate("Device");
+        })
+        .catch(reason => console.log({ reason }));
+      return;
+    }
+
+    BleManager.connect(this.device.id)
+      .then(() => {
+        // Success code
+        console.log("Connected");
+        this.props.navigation.navigate("Device");
+      })
+      .then(
+        BleManager.retrieveServices(this.device.id)
+          .then(deviceInfo => {
+            // Success code
+            console.log({
+              DeviceInfo: deviceInfo
+            });
+          })
+          .catch(reason => console.log({ reason }))
+      )
+      .catch(error => {
+        // Failure code
+        console.log(error);
+      });
+      */
   };
 
   render() {
     return (
-      <TouchableOpacity style={{ padding: 1 }} onPress={this.onPress}>
+      <TouchableOpacity
+        style={{
+          padding: 1
+        }}
+        onPress={this._onPress}
+      >
         <Card containerStyle={styles.list}>
-          <Text>Id: {this.state.id}</Text>
-          <Text>Name: {this.state.name}</Text>
-          <Text>Rssi: {this.state.rssi}</Text>
+          <Text> Id: {this.state.id} </Text>
+          <Text> Name: {this.state.name} </Text>
+          <Text> Rssi: {this.state.rssi} </Text>
         </Card>
       </TouchableOpacity>
     );
@@ -57,6 +179,6 @@ const styles = StyleSheet.create({
   list: {
     padding: 7,
     margin: 10,
-    borderRadius: 10,
+    borderRadius: 10
   }
 });
