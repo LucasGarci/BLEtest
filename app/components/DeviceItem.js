a3fc0569a172dc41a34ecb0d923b9e6b5dd5ddf6
lucas <lucas@jsdarios-MBP.home>
2018-06-13 14:05:31 +0200

    funciona el CAMBIO de un color a otro y he deslinkeado la antigua lib de bluetooth

--- app/components/DeviceItem.js
+++ app/components/DeviceItem.js
@@ -1,95 +1,167 @@
-import React from "react";
-import { StyleSheet, Text, TouchableOpacity } from "react-native";
-import { Card } from "react-native-elements";
-import BleManager from "react-native-ble-manager";
-import Buffer from 'buffer';
+import React from "react"
+import {
+  StyleSheet, Text, TouchableOpacity, NativeEventEmitter,
+  NativeModules,
+} from "react-native"
+import { Card } from "react-native-elements"
+import BleManager from "react-native-ble-manager"
+import Buffer from "buffer"
+import { bytesToString } from 'convert-string';
+
+const BleManagerModule = NativeModules.BleManager
+//We create our events emitter
+const bleManagerEmitter = new NativeEventEmitter(BleManagerModule)
+
+var conversor = require("convert-hex")
+
+// DONT DELETE THIS COMMENT https://www.npmjs.com/package/convert-hex
 
 export class DeviceItem extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props)
     this.state = {
       id: null,
       name: null,
       rssi: null,
       connected: false,
-      peripherals: new Map()
-    };
-    this.device = {};
+      devices: new Map()
+    }
+    this.device = {}
+    this.oldData = [
+      76,
+      69,
+      68,
+      66,
+      76,
+      69,
+      45,
+      55,
+      56,
+      54,
+      50,
+      57,
+      66,
+      52,
+      51,
+      0
+    ]
+    this.handleUpdateValueForCharacteristic = this.handleUpdateValueForCharacteristic.bind(
+      this
+    )
   }
 
   componentDidMount() {
-    const { device } = this.props;
+    const { device } = this.props
     this.setState({
       id: device.id,
       name: device.name || "Anonimo",
       rssi: device.rssi,
       connected: device.connected
+    })
+    this.device = this.props.device
+    this.handlerUpdate = bleManagerEmitter.addListener(
+      "BleManagerDidUpdateValueForCharacteristic",
+      this.handleUpdateValueForCharacteristic
+    )
+  }
 
-    });
-    this.device = this.props.device;
+  handleUpdateValueForCharacteristic(data) {
+    console.log('Received data from ' + data.peripheral + ' characteristic ' + data.characteristic, data.value);
   }
 
   _onPress = () => {
-    const peripheral = this.props.device;
-    if (peripheral) {
-      console.log("Connected to " + peripheral.id);
-      if (peripheral.connected) {
-        BleManager.disconnect(peripheral.id);
-      } else {
-        BleManager.connect(peripheral.id)
-          .then(() => {
-            let peripherals = this.state.peripherals;
-            let p = peripherals.get(peripheral.id);
-            if (p) {
-              p.connected = true;
-              peripherals.set(peripheral.id, p);
-              this.setState({ peripherals });
-            }
-            console.log("Connected to " + peripheral.id);
+    console.log({ props: this.props })
+    const { device } = this.props
+    if (device) {
+      console.log("Connected to " + device.id)
+      BleManager.connect(device.id)
+        .then(() => {
+          let devices = this.state.devices
+          let p = devices.get(device.id)
+          if (p) {
+            p.connected = true
+            devices.set(device.id, p)
+            this.setState({ devices })
+          }
+          console.log("Connected to " + device.id)
 
-            setTimeout(() => {
-              BleManager.retrieveServices(peripheral.id)
-                .then(peripheralInfo => {
-                  console.log({ peripheralInfo });
-                  BleManager.startNotification(
-                    peripheralInfo.id,
-                    peripheralInfo.characteristics[0].service,
-                    peripheralInfo.characteristics[0].characteristic
-                  );
-                  console.log("lanzado peripheral infos!");
-                  return peripheralInfo;
-                })
-                .then(peripheralInfo => {
-                  BleManager.read(
-                    peripheralInfo.id,
-                    peripheralInfo.characteristics[0].service,
-                    peripheralInfo.characteristics[0].characteristic
+          setTimeout(() => {
+            BleManager.retrieveServices(device.id)
+              .then(deviceInfo => {
+                console.log({ deviceInfo })
+                // Esta es la unica caracteristica que responde a startNotific...
+                BleManager.startNotification(
+                  deviceInfo.id,
+                  deviceInfo.characteristics[4].service,
+                  deviceInfo.characteristics[4].characteristic
+                ).catch(errNotif => console.log({ errNotif }))
+                console.log("lanzado device infos!")
+                return deviceInfo
+              })
+              .then(async deviceInfo => {
+                // BleManager.read(
+                //   deviceInfo.id,
+                //   deviceInfo.characteristics[0].service,
+                //   deviceInfo.characteristics[0].characteristic
+                // )
+                //   .then(readData => {
+                // Success code
+                // console.log({ readData })
+                // const buffer = Buffer.Buffer.from(readData)
+                // console.log({ buffer })
+                // const sensorData = buffer.readUInt8(1, true)
+                // console.log({ sensorData })
+                // Turn Off the Light
+                await BleManager.createBond(deviceInfo.id)
+                  .then(() => {
+                    console.log(
+                      "createBond success or there is already an existing one"
+                    )
+                  })
+                  .catch(() => {
+                    console.log("fail to bond")
+                  })
+                /*
+                Red : 0x56ff000000f0aa
+                Yellow : 0x56ffff0000f0aa,
+                Blue: 0x560000ff00f0aa
+                */
+                const data = conversor.hexToBytes('0x56ffff0000f0aa')
+                console.log({ dataToWrite: data })
+                for (let index = 0; index < 4; index++) {
+                  await BleManager.write(
+                    deviceInfo.id,
+                    deviceInfo.characteristics[3].service,
+                    deviceInfo.characteristics[3].characteristic,
+                    data
                   )
-                    .then(readData => {
+                    .then(() => {
                       // Success code
-                      console.log({readData});
-                      const buffer = Buffer.Buffer.from(readData);
-                      console.log({ buffer });
-                      const sensorData = buffer.readUInt8(1, true);
-                      console.log({ sensorData });
+                      console.log("Writed: " + data)
                     })
                     .catch(error => {
                       // Failure code
-                      console.log(error);
-                    });
-                });
-            }, 900);
-          })
-          .catch(error => {
-            console.log("Connection error", error);
-          });
-      }
+                      console.log(error)
+                    })
+                }
+
+                // })
+                // .catch(error => {
+                //   // Failure code
+                //   console.log(error)
+                // })
+              })
+          }, 900)
+        })
+        .catch(error => {
+          console.log("Connection error", error)
+        })
     }
     /*  BleManager.stopScan().then(() => {
       // Success code
       console.log("Scan stopped");
     });
-
+   
     if (this.props.device.connected) {
       console.log("Device is connected");
       BleManager.retrieveServices(this.device.id)
@@ -103,7 +175,7 @@ export class DeviceItem extends React.Component {
         .catch(reason => console.log({ reason }));
       return;
     }
-
+   
     BleManager.connect(this.device.id)
       .then(() => {
         // Success code
@@ -125,7 +197,7 @@ export class DeviceItem extends React.Component {
         console.log(error);
       });
       */
-  };
+  }
 
   render() {
     return (
@@ -138,10 +210,10 @@ export class DeviceItem extends React.Component {
         <Card containerStyle={styles.list}>
           <Text> Id: {this.state.id} </Text>
           <Text> Name: {this.state.name} </Text>
-          <Text> Rssi: {this.state.rssi} </Text>
+          <Text> Rssi: {this.state.rssi || 'Connected'} </Text>
         </Card>
       </TouchableOpacity>
-    );
+    )
   }
 }
 
@@ -151,4 +223,4 @@ const styles = StyleSheet.create({
     margin: 10,
     borderRadius: 10
   }
-});
+})
