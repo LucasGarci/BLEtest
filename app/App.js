37be08246de703e66ebddb7a5e441e51b53e9e93
LucasGarci <lucasgarcifer@gmail.com>
2018-06-14 15:14:48 +0200

    Translator1 (#29)
    * empezando translator
    
    * installed i18n
    
    * translations-working

--- app/App.js
+++ app/App.js
@@ -1,6 +1,8 @@
 import React from "react"
 import { Root } from "./navigation/router"
 
+var translator = require('../I18n/I18n.js');
+
 class App extends React.Component {
   render() {
     console.disableYellowBox = true;
