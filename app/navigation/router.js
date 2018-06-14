b248fce34c94866206d3f77266ec0868388540e5
unknown <lucasgarcifer@gmail.com>
2018-06-14 02:03:09 +0200

    colorwheel is working but get frozen in early tasks

--- app/navigation/router.js
+++ app/navigation/router.js
@@ -1,18 +1,16 @@
-import React from "react"
-import { TabNavigator, StackNavigator } from "react-navigation"
-import { Text } from "react-native"
-import { Icon } from "react-native-elements"
+import React from "react";
+import { TabNavigator, StackNavigator } from "react-navigation";
+import { Text } from "react-native";
+import { Icon } from "react-native-elements";
 
-import { Scanner } from "../Scanner"
-import { BriTab } from "../containers/BriTab"
-import { ColorTab } from "../containers/ColorTab"
-import { TempTab } from "../containers/TempTab"
-import { EffectTab } from "../containers/EffectTab"
-import { OptionsScreen } from "../containers/OptionsScreen"
-import { DeviceTabs } from "../containers/DeviceTabs"
-import { OptionsButton } from "../components/OptionsButton"
-
-
+import { Scanner } from "../Scanner";
+import { BriTab } from "../containers/BriTab";
+import { ColorTab } from "../containers/ColorTab";
+import { TempTab } from "../containers/TempTab";
+import { EffectTab } from "../containers/EffectTab";
+import { OptionsScreen } from "../containers/OptionsScreen";
+import { DeviceTabs } from "../containers/DeviceTabs";
+import { OptionsButton } from "../components/OptionsButton";
 
 export const Root = StackNavigator({
   Home: {
@@ -34,47 +32,31 @@ export const Root = StackNavigator({
       title: "Device Controller"
     }
   }
-})
+});
 
 export const DeviceNavigation = TabNavigator({
   Brigthness: {
     screen: BriTab,
     navigationOptions: {
-      tabBarLabel: <Icon
-        name='brightness-6'
-        color='white'
-        size={32}
-      />
+      tabBarLabel: <Icon name="brightness-6" color="white" size={32} />
     }
   },
   RGB: {
     screen: ColorTab,
     navigationOptions: {
-      tabBarLabel: <Icon
-      name='color-lens'
-      color='white'
-      size={32}
-    />
+      tabBarLabel: <Icon name="color-lens" color="white" size={32} />
     }
   },
   Temperature: {
     screen: TempTab,
     navigationOptions: {
-      tabBarLabel: <Icon
-      name='beach-access'
-      color='white'
-      size={32}
-    />
+      tabBarLabel: <Icon name="beach-access" color="white" size={32} />
     }
   },
   Effect: {
     screen: EffectTab,
     navigationOptions: {
-      tabBarLabel: <Icon
-      name='smoking-rooms'
-      color='white'
-      size={32}
-    />
+      tabBarLabel: <Icon name="smoking-rooms" color="white" size={32} />
     }
   }
-})
+});
