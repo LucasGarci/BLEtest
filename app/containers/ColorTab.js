d39c8e5c9636dcbc5a993990bebf68eb02df0124
Desfase <kikdesfase@gmail.com>
2018-06-15 22:31:14 +0200

    on off pero mal

--- app/containers/ColorTab.js
+++ app/containers/ColorTab.js
@@ -7,9 +7,10 @@ import {
   TouchableOpacity,
   Dimensions,
   NativeEventEmitter,
-  NativeModules,
+  NativeModules
 } from "react-native";
 import { ColorWheel } from "react-native-color-wheel";
+import { hsv2Rgb } from "colorsys";
 import { PrefabPicker } from "../components/PrefabPicker";
 import BleManager from "react-native-ble-manager";
 
@@ -28,6 +29,7 @@ export class ColorTab extends React.Component {
       deviceInfo: {},
       lastUpdateValue: {}
     };
+    this.writing = false;
     this.handleBleManagerDidUpdateState = this.handleBleManagerDidUpdateState.bind(
       this
     );
@@ -59,23 +61,23 @@ export class ColorTab extends React.Component {
     this.setState({ lastUpdateValue: data });
   }
 
-  async handleColorChange(color) {
+verifications() {
+}
+
+async handleOnPress() {
     BleManager.checkState();
-    const rgbColor = hsv2Rgb(color.h, color.s, color.v);
-    this.setState({ currentColor: rgbColor });
-    console.log({ colorIs: this.state.currentColor });
     const device = this.state.devicesConnected[0];
     console.log({ CT_devConected: device });
 
     //Sacamos los servicios
     await BleManager.retrieveServices(device.id)
-      .then(deviceInfo => {
-        console.log({ CT_devInf: deviceInfo });
-        this.setState({ deviceInfo });
-      })
-      .catch(error => {
-        console.log(error);
-      });
+    .then(deviceInfo => {
+      console.log({ CT_devInf: deviceInfo });
+      this.setState({ deviceInfo });
+    })
+    .catch(error => {
+      console.log(error);
+    });
 
     const deviceInfo = this.state.deviceInfo;
     // Esta es la unica caracteristica que responde a startNotific...
@@ -94,19 +96,9 @@ export class ColorTab extends React.Component {
         console.log("Fail to bond");
       });
 
-    const mode = "0x56";
-    // Debemos pasar la cadena a hexadecimal y que sea un valor de dos dígitos
-    const r = secureByte(rgbColor.r.toString(16));
-    const g = secureByte(rgbColor.g.toString(16));
-    const b = secureByte(rgbColor.b.toString(16));
-    //concatenamos mode+r+g+b+constant
-    const colorToWrite = mode.concat(r, g, b, "00f0aa");
-    console.log({ colorToWrite: colorToWrite });
-
-    const data = conversor.hexToBytes(colorToWrite);
-    console.log({ dataToWrite: data });
-
-    //  BleManager.checkState
+    const off = "CC2333" 
+    const data = conversor.hexToBytes(off);
+    
     await BleManager.write(
       deviceInfo.id,
       deviceInfo.characteristics[3].service,
@@ -122,6 +114,78 @@ export class ColorTab extends React.Component {
         // Failure code
         console.log(error);
       });
+}
+
+  async handleColorChange(color) {
+    if (!this.writing) {
+      this.writing = true;
+      BleManager.checkState();
+      const rgbColor = hsv2Rgb(color.h, color.s, color.v);
+      this.setState({ currentColor: rgbColor });
+      console.log({ colorIs: this.state.currentColor });
+      const device = this.state.devicesConnected[0];
+      console.log({ CT_devConected: device });
+
+      //Sacamos los servicios
+      await BleManager.retrieveServices(device.id)
+        .then(deviceInfo => {
+          console.log({ CT_devInf: deviceInfo });
+          this.setState({ deviceInfo });
+        })
+        .catch(error => {
+          console.log(error);
+        });
+
+      const deviceInfo = this.state.deviceInfo;
+      // Esta es la unica caracteristica que responde a startNotific...
+      await BleManager.startNotification(
+        deviceInfo.id,
+        deviceInfo.characteristics[4].service,
+        deviceInfo.characteristics[4].characteristic
+      ).catch(errNotif => console.log({ errNotif }));
+
+      //Creamos enlace o certificamos que esta creado
+      await BleManager.createBond(deviceInfo.id)
+        .then(() => {
+          console.log("Bonding its OK");
+        })
+        .catch(() => {
+          console.log("Fail to bond");
+        });
+
+      const mode = "0x56";
+      // Debemos pasar la cadena a hexadecimal y que sea un valor de dos dígitos
+      const r = secureByte(rgbColor.r.toString(16));
+      const g = secureByte(rgbColor.g.toString(16));
+      const b = secureByte(rgbColor.b.toString(16));
+      //concatenamos mode+r+g+b+constant
+      const colorToWrite = mode.concat(r, g, b, "00f0aa");
+      console.log({ colorToWrite: colorToWrite });
+
+      const data = conversor.hexToBytes(colorToWrite);
+      console.log({ dataToWrite: data });
+
+      //  BleManager.checkState
+
+      await BleManager.write(
+        deviceInfo.id,
+        deviceInfo.characteristics[3].service,
+        deviceInfo.characteristics[3].characteristic,
+        data
+      )
+        .then(() => {
+          index = 10;
+          // Success code
+          console.log("Writed: " + data);
+        })
+        .catch(error => {
+          // Failure code
+          console.log(error);
+        });
+      this.writing = false;
+    } else {
+      console.log("no ha escrito");
+    }
   }
 
   render() {
@@ -138,6 +202,18 @@ export class ColorTab extends React.Component {
         <View style={styles.centerContainer}>
           <PrefabPicker color={this.state.currentColor} />
         </View>
+        <View style={styles.buttonContainer}>
+          <Button
+            title="ON"
+            onPress={() => this.handleOnPress("ON")}
+            style={styles.textOption}
+          />
+          <Button
+            title="OFF"
+            onPress={() => this.handleOnPress("OFF")}
+            style={styles.textOption}
+          />
+        </View>
       </View>
     );
   }
@@ -145,6 +221,11 @@ export class ColorTab extends React.Component {
 
 const styles = StyleSheet.create({
   centerContainer: {
+    flex: 2,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  buttonContainer: {
     flex: 1,
     justifyContent: "center",
     alignItems: "center"
@@ -157,6 +238,11 @@ const styles = StyleSheet.create({
     height: 30,
     width: 30,
     borderRadius: 10
+  },
+  textOption: {
+    fontSize: 16,
+    fontWeight: "bold",
+    color: "#606060"
   }
 });
 
@@ -166,37 +252,3 @@ secureByte = function(byteString) {
   }
   return byteString;
 };
-
-hsv2Rgb = function(h, s, v) {
-  if (typeof h === "object") {
-    const args = h;
-    h = args.h;
-    s = args.s;
-    v = args.v;
-  }
-
-  h = _normalizeAngle(h);
-  h = h === 360 ? 1 : ((h % 360) / parseFloat(360)) * 6;
-  s = s === 100 ? 1 : (s % 100) / parseFloat(100);
-  v = v === 100 ? 1 : (v % 100) / parseFloat(100);
-
-  var i = Math.floor(h);
-  var f = h - i;
-  var p = v * (1 - s);
-  var q = v * (1 - f * s);
-  var t = v * (1 - (1 - f) * s);
-  var mod = i % 6;
-  var r = [v, q, p, p, t, v][mod];
-  var g = [t, v, v, q, p, p][mod];
-  var b = [p, p, t, v, v, q][mod];
-
-  return {
-    r: Math.floor(r * 255),
-    g: Math.floor(g * 255),
-    b: Math.floor(b * 255)
-  };
-};
-
-function _normalizeAngle(degrees) {
-  return ((degrees % 360) + 360) % 360;
-}
