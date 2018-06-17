7787e922ad17af289f2d7a2d3c089c989a2f04bb
LucasGarci <lucasgarcifer@gmail.com>
2018-06-17 18:06:59 +0200

    starting (#54)

--- app/redux/store.js
+++ app/redux/store.js
@@ -1,13 +1,29 @@
-import { createStore, applyMiddleware, compose } from 'redux'
-import globalReducer from './reducers'
-import { initialState } from './initialState'
+import { createStore, applyMiddleware } from "redux";
+import globalReducer from "./reducers";
+import { initialState } from "./initialState";
+import { AsyncStorage } from "react-native";
+import { createStore } from "redux";
+import { persistStore, persistReducer, autoRehydrate } from "redux-persist";
 
-const needThisToDebugState = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+const needThisToDebugState =
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+
+const persistConfig = {
+  key: "root",
+  AsyncStorage
+};
+
+const persistedReducer = persistReducer(persistConfig, globalReducer);
+
+export default () => {
+  let store = createStore(persistedReducer);
+  let persistor = persistStore(store);
+  return { store, persistor };
+};
 
 // createStore (reductores,<estadoInicial>,<mejoras>)
 export const store = createStore(
-    globalReducer,
-    initialState,
-    needThisToDebugState)
-
-console.log(store.getState())
+  persistedReducer,
+  initialState,
+  composeEnhancers(applyMiddleware(...needThisToDebugState), autoRehydrate())
+);
